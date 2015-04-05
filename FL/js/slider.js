dffe74df212ba2b755a5cf443edbe8418bf1fe45
Halina Mader <h.mader@umiami.edu>
2015-04-05 15:31:53 -0400

    map layering with one map

--- FL/js/slider.js
+++ FL/js/slider.js
@@ -1,7 +1,10 @@
+var increments = 10;
+
+
 $("#slider").slider({
     orientation: "horizontal",
     min: 0,
-    max: 10,
+    max: 100,
     change: switchSVG
 });
 
@@ -9,12 +12,13 @@ $("#slider").slider({
 
 function switchSVG(event, ui) {
     var level = $("#slider").slider("option", "value");
+    level = Math.round(level/increments);
     if (level == 0) {
-        level = 1
-    };
+        $("#fl-sea-level > img").attr("src", "assets/FL-base.svg");
+    } else {
 
-    $("#fl-sea-level").attr("src", "assets/FL_" + level + "ft.svg");
-
+    $("#fl-sea-level > img").attr("src", "assets/FL_" + level + "ft.svg");
+    }
     console.log("level: " + level);
 }
 
