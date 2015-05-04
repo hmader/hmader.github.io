83b04d9233b42e07e0831352197ce057a65dc940
Halina Mader <h.mader@umiami.edu>
2015-05-04 11:32:18 -0400

    pretty much done minus complete data & a few minor ui pieces. Need to finalize desing

--- FL/js/main.js
+++ FL/js/main.js
@@ -40,7 +40,7 @@ var legislation = [{
         "key": "3",
     "abbr" : "H.R. 3 (2015)",
         "title": "H.R. 3, Keystone XL Pipeline Act",
-        "summary": "<p>Authorizes TransCanada Keystone Pipeline, L.P. to construct, connect, operate, and maintain the pipeline and cross-border facilities specified in an application filed by TransCanada Corporation to the Department of State on May 4, 2012. (Congress.gov)</p><p><strong>A vote for this legislation supports the Keystone Pipeline, which will lead to mass transfer of oil through North America and expansion of tar sands development. This will further promote the high use of fossil fuels and carbon pollution, contributing to the speed of climate change, warming ocean water, ice melt, and sea level rise.</strong></p>"
+        "summary": "<p>Authorizes TransCanada Keystone Pipeline, L.P. to construct, connect, operate, and maintain the pipeline and cross-border facilities specified in an application filed by TransCanada Corporation to the Department of State on May 4, 2012. (Congress.gov)</p><p><strong>A vote FOR this legislation supports the Keystone Pipeline, which will lead to mass transfer of oil through North America and expansion of tar sands development. This will further promote the high use of fossil fuels and carbon pollution, contributing to the speed of climate change, warming ocean water, ice melt, and sea level rise.</strong></p>"
 
 },
     {
@@ -53,49 +53,49 @@ var legislation = [{
         "key": "48",
      "abbr" : "H.Res 48 (2015)",
         "title": "Providing for consideration of the bill (H.R. 351)",
-        "summary": "'To provide for expedited approval of exportation of natural gas.' (Congress.gov)"
+        "summary": "<p>To provide for expedited approval of exportation of natural gas. (Congress.gov)</p><p><strong></strong></p>"
 
 },
     {
         "key": "2231",
          "abbr" : "H.R. 2231 (2013)",
         "title": "H.R. 2231, the Offshore Energy and Jobs Act",
-        "summary": "Amends the Outer Continental Shelf Lands Act (OCSLA) to direct the Secretary of the Interior to implement a leasing program that includes at least 50% of the available unleased acreage within each outer Continental Shelf (OCS) planning area considered to have the largest undiscovered, technically recoverable oil and gas resources, with an emphasis on offering the most geologically prospective parts of the planning area. Instructs the Secretary, in developing a five-year oil and gas leasing program, to determine a specified domestic strategic production goal for the development of oil and natural gas as a result of that program. Requires the Secretary to: (1) develop and submit to Congress a new five-year oil and gas leasing program. (2) conduct offshore oil and gas Lease Sale 220 within one year after enactment of this Act, and (3) make replacement lease blocks available in the Virginia lease sale planning area that are acceptable for oil and gas exploration and production if the Secretary of Defense proposes deferral from a lease offering due to defense-related activities irreconcilable with mineral exploration and development. (Congress.gov)"
+        "summary": "<p>Amends the Outer Continental Shelf Lands Act (OCSLA) to direct the Secretary of the Interior to implement a leasing program that includes at least 50% of the available unleased acreage within each outer Continental Shelf (OCS) planning area considered to have the largest undiscovered, technically recoverable oil and gas resources, with an emphasis on offering the most geologically prospective parts of the planning area. Instructs the Secretary, in developing a five-year oil and gas leasing program, to determine a specified domestic strategic production goal for the development of oil and natural gas as a result of that program. Requires the Secretary to: (1) develop and submit to Congress a new five-year oil and gas leasing program. (2) conduct offshore oil and gas Lease Sale 220 within one year after enactment of this Act, and (3) make replacement lease blocks available in the Virginia lease sale planning area that are acceptable for oil and gas exploration and production if the Secretary of Defense proposes deferral from a lease offering due to defense-related activities irreconcilable with mineral exploration and development. (Congress.gov)</p><p><strong>A vote FOR this legislation supports expanding offshore drilling off the coasts of South Carolina, Virginia, California, and in Alaska’s Bristol Bay. The use of fossil fuels contributes to pollution and increased quantities of greenhouse gasses in the atmosphere, contributing to the speed of climate change, ice melt, and sea level rise.</strong></p>"
 
 },
     {
         "key": "2728",
          "abbr" : "H.Amdt 507 (2013)",
         "title": "Amendment to H.R. 2728, the Protecting States’ Rights to Promote American Energy Security Act",
-        "summary": "'Amendment sought to allow the Secretary of the Interior to issue regulations to reduce methane emissions from oil and gas drilling operations on public lands.' (Congress.gov)"
+        "summary": "<p>Amendment sought to allow the Secretary of the Interior to issue regulations to reduce methane emissions from oil and gas drilling operations on public lands. (Congress.gov)</p><p><strong>A vote FOR this amendment is a vote against the regulation of methane, a greenhouse gas and pollutant that has over 20 times the effect of carbon dioxide. Large quantities of methane in the atmosphere contributes to increased warming, which leads to ice melt and sea level rise, as well as contributing to smog. This amendment failed to pass the House on November 20.</strong></p>"
 
 },
     {
         "key": "3826a",
          "abbr" : "H.Amdt 581 (2014)",
         "title": "Amendment to H.R. 3826, the Electricity Security and Affordability Act",
-        "summary": "<p>An amendment numbered 6 printed in House Report 113-373 to accept the scientific finding of the EPA that greenhouse gas pollution is 'contributing to long-lasting changes in our climate that can have a range of negative effects.' (Congress.gov)</p><p><strong>A vote against this amendment suggests the denial that greenhouse gasses impact climate change and the negative results of climate change.</p> <p>It has been scientifically shown that green house gasses trap heat in our atmosphere, which in turn increases global average temperature. In addition to effects such as drought and forest fires, this leads to warmer ocean water and melting ice sheets, which causes sea level rise. This amendment failed to pass the House on March 6.<strong></p> "
+        "summary": "<p>An amendment numbered 6 printed in House Report 113-373 to accept the scientific finding of the EPA that greenhouse gas pollution is 'contributing to long-lasting changes in our climate that can have a range of negative effects.' (Congress.gov)</p><p><strong>A vote AGAINST this amendment suggests the denial that greenhouse gasses impact climate change and the negative results of climate change.</p> <p>It has been scientifically shown that green house gasses trap heat in our atmosphere, which in turn increases global average temperature. In addition to effects such as drought and forest fires, this leads to warmer ocean water and melting ice sheets, which causes sea level rise. This amendment failed to pass the House on March 6.<strong></p> "
 
 },
     {
         "key": "3826",
          "abbr" : "H.R. 3826 (2014)",
         "title": "H.R. 3826, the Electricity Security and Affordability Act",
-        "summary": "Prohibits the Administrator of the Environmental Protection Agency (EPA) from issuing, implementing, or enforcing any proposed or final rule under the Clean Air Act that establishes a performance standard for greenhouse gas emissions from any new source that is a fossil fuel-fired electric utility generating unit unless the rule meets specified requirements of this Act. (Congress.gov)"
+        "summary": "<p>Prohibits the Administrator of the Environmental Protection Agency (EPA) from issuing, implementing, or enforcing any proposed or final rule under the Clean Air Act that establishes a performance standard for greenhouse gas emissions from any new source that is a fossil fuel-fired electric utility generating unit unless the rule meets specified requirements of this Act. (Congress.gov)</p><p><strong>A vote FOR this legislation is a vote to prevent the EPA from limiting carbon emissions from power plants, the nation’s largest source of the pollution driving climate change. The devastating impacts of climate change include more frequent and severe weather events like droughts, wildfires, floods, and storms. Climate change also threatens public health by increasing smog, which triggers asthma attacks and is especially dangerous for children and those with heart or lung disease. The U.S. already places limits on mercury, arsenic, particulate matter, lead and other pollution from power plants, but this extreme legislation would ensure that power plants remain free to release unlimited amounts of carbon pollution into our air. On March 6, the House approved this Bill.**</strong></p>"
 
 },
     {
         "key": "2397a",
         "abbr" : "H.Amdt 380 (2013)",
         "title": "Amendment to H.R. 2397, the Department of Defense Appropriations Act, 2014",
-        "summary": "Amendment prohibits the use of funds in the bill to enforce section 526 of the Energy Independence and Security Act, which prohibits federal agencies from entering into contracts for the procurement of an alternative fuel unless its lifecycle greenhouse gas emissions are less than or equal to emissions from an equivalent conventional fuel produced from conventional petroleum sources. (Congress.gov)"
+        "summary": "<p>Amendment prohibits the use of funds in the bill to enforce section 526 of the Energy Independence and Security Act, which prohibits federal agencies from entering into contracts for the procurement of an alternative fuel unless its lifecycle greenhouse gas emissions are less than or equal to emissions from an equivalent conventional fuel produced from conventional petroleum sources. (Congress.gov)</p><p><strong>A vote FOR this amendment supports the defunding of the federal government’s prohibition on using unconventional transportation fuels that emit more greenhouse gases than traditional fuels. The amendment would prohibit the use of funds in the bill to enforce Section 526 of the Energy Independence and Security Act of 2007, which specifically prohibits federal agencies from buying transportation fuels from unconventional sources like coal-to-liquid, oil sands, and oil shale, unless the contract specifies that the lifecycle greenhouse gas emissions do not exceed those produced by conventional petroleum. On July 23, the House approved this amendment.**</strong></p>"
 
 },
     {
         "key": "1582a",
         "abbr" : "H.Amdt 447 (2013)",
         "title": "Amendment to H.R. 1582, the Energy Consumers Relief Act of 2013",
-        "summary": "Amendment prohibits the EPA from using the 'social cost of carbon' valuation for any energy-related rule that costs more than $1 billion unless and until a Federal law is enacted authorizing such use. (Congress.gov)"
+        "summary": "<p>Amendment prohibits the EPA from using the 'social cost of carbon' valuation for any energy-related rule that costs more than $1 billion unless and until a Federal law is enacted authorizing such use. (Congress.gov)</p><strong>A vote FOR this amendment would support forcing the federal government to ignore the economic costs of climate change. Climate change is already costing communities billions of dollars each year across the country, and this amendment would prevent the government from weighing the costs of extreme weather and other climate change impacts or the savings from any government actions to improve energy efficiency and clean up carbon pollution. On August 1, the House approved this amendment.**</strong></p>"
 
 },
     {
@@ -123,7 +123,7 @@ var legislation = [{
         "key": "4923b",
         "abbr" : "H.Amdt 1015 (2014)",
         "title": "Amendment to H.R. 4923, the Energy and Water Development and Related Agencies Appropriations Act, 2015",
-        "summary": "<p>Amendment adds a new section prohibiting the use of funds to prepare, propose, or promulgate any regulation or guidance that references or relies on the analysis contained in specified documents on the subject of the Social Cost of Carbon. (Congress.gov)</p><p><strong>A vote for this amendment supports federal agencies' ignoring the economic costs of climate change. It prevents the government from considering the potential costs of the effects of climate change, including extreme weather and flooding due to sea level rise. This amendment was approved by the House on July 10, although it was not included in the final bill that was signed into law on December 16.</strong></p>"
+        "summary": "<p>Amendment adds a new section prohibiting the use of funds to prepare, propose, or promulgate any regulation or guidance that references or relies on the analysis contained in specified documents on the subject of the Social Cost of Carbon. (Congress.gov)</p><p><strong>A vote FOR this amendment supports federal agencies' ignoring the economic costs of climate change. It prevents the government from considering the potential costs of the effects of climate change, including extreme weather and flooding due to sea level rise. This amendment was approved by the House on July 10, although it was not included in the final bill that was signed into law on December 16.</strong></p>"
 
 },
 
@@ -867,6 +867,7 @@ $(".show").click(function() {
     $("#fl-districts").click(function () {
         console.log("clicked");
         $("#district-wrapper").css("visibility", "visible");
+        $("#district-wrapper").css("display", "flex");
         $('html,body').animate({
         scrollTop: $("#district-wrapper").offset().top},
         'slow');
@@ -896,7 +897,7 @@ $(".show").click(function() {
             $("path").css({"fill": "#fff"});
             $("#district-rep").html(districts[districtNum - 1].representative.name + ", " + districts[districtNum - 1].title);
             $("#rep-photo > img").attr("src", photoPath);
-            $("#district-counties").html(districts[districtNum - 1].counties);
+            $("#district-counties").html(districts[districtNum - 1].counties + " Counties");
             
             
             var selectedHistory = districts[districtNum - 1].representative.votingHistory;
@@ -986,7 +987,7 @@ $(".show").click(function() {
                      } else if(selectedHistory[i].vote == "n") {
                        $("#voted").html("Voted AGAINST"); 
                      } else {
-                       $("#voted").html("No recorded vote"); 
+                       $("#voted").html("No recorded vote - Representative either not in office at time of legislation or chose not to vote"); 
                      }
                  }
             }
