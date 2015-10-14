3ecac3647e8847f103a72e839dd52a2b3f302459
hmader <halina.mader@gmail.com>
2015-10-14 01:20:54 -0400

    adding portfolio

---
+++ Portfolio/js/myBackstretch.js
@@ -0,0 +1,8 @@
+$(".backstretch-div").backstretch([
+    "images/AnniBWimg.png",
+    "images/AnniStationery.png",
+    "images/AnniNight1.png",
+    "images/AnniNight2.png",   
+    "images/AnniColorimg.png",   
+    "images/AnniSimply.png"   
+  ], {duration: 4000});
